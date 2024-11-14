1737021ced6d6a1f197a7b51494433ea495e5af1
salmanfxrsi <salmanxprivate@gmail.com>
2024-11-14 15:46:54 +0600

    feat: added ErrorPage component and set it as the error element in routes

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import Header from "./Header";
+
+const ErrorPage = () => {
+    return (
+        <div className="mt-10">
+            <h1 className="text-3xl font-bold text-center mb-10">No Page Found</h1>
+            <Header></Header>
+            <Link to={'/'} className="text-3xl font-bold flex justify-center mt-4 text-slate-500">Click Here To Go Back To Home</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomeLayout from "../layouts/HomeLayout";
+import ErrorPage from "../components/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <ErrorPage />
   },
 ]);
 
